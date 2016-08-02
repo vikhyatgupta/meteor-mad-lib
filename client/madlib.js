a40041cfcc7e98ccc5b4b5a40df4bcfa93e70d7c
mahdi saeikhalili <mahdi.saeikhalili@gmail.com>
2016-08-01 20:25:21 -0400

    stored the text values

--- client/madlib.js
+++ client/madlib.js
@@ -2,5 +2,12 @@ Template.body.events({
 	'submit form': function(event, template){
 		event.preventDefault();
 		console.log("Hello, World");
+		let exclamation = template.find('#exclamation').value;
+		let adverb = template.find('#adverb').value;
+		let noun = template.find('#noun').value;
+		let verb= template.find('#verb').value;
+		let madlib = exclamation +" you say, as you " + adverb +
+		", jump onto your "+ noun + " and "+ verb + " into the sunset.";
+		console.log(madlib);
 	}
 });
