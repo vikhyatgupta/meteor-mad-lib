c680fccc0d83d2bb51064e88b9cc37bb869d4f4d
Vikhyat Gupta <vikhyatgupta@gmail.com>
2016-08-01 23:14:38 -0400

    added animation effect

--- client/madlib.js
+++ client/madlib.js
@@ -1,13 +1,20 @@
+import { Template } from 'meteor/templating';
+
 Template.body.events({
-	'submit form': function(event, template){
-		event.preventDefault();
-		console.log("Hello, World");
-		let exclamation = template.find('#exclamation').value;
-		let adverb = template.find('#adverb').value;
-		let noun = template.find('#noun').value;
-		let verb= template.find('#verb').value;
-		let madlib = exclamation +" you say, as you " + adverb +
-		", jump onto your "+ noun + " and "+ verb + " into the sunset.";
-		console.log(madlib);
-	}
+  'submit .form-horizontal'(event, template) {
+    event.preventDefault();
+
+    // console.log('Hello, world');
+    let exclamation = template.find("input[name=exclamation]").value;
+    let adverb = template.find("input[name=adverb]").value;
+    let noun = template.find("input[name=noun]").value;
+    let verb = template.find("input[name=verb]").value;
+
+    const madlib = `"${exclamation}!", you say as you ${adverb} jump onto your ${noun} and ${verb} off into the sunset`;
+
+    $('#madlib').hide();
+    $('#madlib').html(madlib);
+    $('#madlib').css('color', '#0000ff');
+    $('#madlib').fadeIn('slow', function() {});
+  },
 });
